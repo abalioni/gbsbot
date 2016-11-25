baf458328644f83732ca248a981ee356c3fcfff2
Aléxia Balionis <balionisalexia@gmail.com>
2016-11-24 22:23:25 -0200

    Updates

--- client/scripts/chat-index.js
+++ client/scripts/chat-index.js
@@ -3,13 +3,15 @@
 
     var button = document.getElementById('askButton'),
         chat = document.getElementById("chat"),
-        meVar = document.getElementById("me") ;
+        meVar = document.getElementById("me"),
+        youVar = document.getElementById("you"),
+        chatHistory = document.getElementById("chatHistory");
 
 
     button.addEventListener("click", function () {
 
-        var input = document.getElementById('question').value;
-        //addQuestion(input);
+        var input = document.getElementById('question');
+        addQuestion(input.value);
 
         fetch("/sendMessage", {
             method: 'post',
@@ -30,30 +32,34 @@
                         console.log(error);
                     });
 
-
+        input.value = "";
 
     });
 
 
-        function addResponse (text) {
-            var p = document.createElement("p"),
-                docFragment = document.createDocumentFragment();
+        function addQuestion (text) {
+            var span = document.createElement("span"),
+                p = document.createElement("p");
+                span.setAttribute("id", "you");
+                var docFragment = document.createDocumentFragment();
+                span.appendChild(p);
                 p.appendChild(document.createTextNode(text));
-                docFragment.appendChild(p);
-                meVar.appendChild(docFragment);
+                docFragment.appendChild(span);
+                chatHistory.appendChild(docFragment);
         };
 
-        // function addResponse (text) {
-        //     var span = document.createElement("span"),
-        //         p = document.createElement("p");
-        //         span.setAttribute("id", "me");
-        //         docFragment = document.createDocumentFragment();
-        //         span.appendChild(p);
-        //         p.appendChild(document.createTextNode(text));
-        //         docFragment.appendChild(span);
-        //         chat.appendChild(docFragment);
-        //
-        //     console.log(text);
-        // };
+        function addResponse (text) {
+
+            var span = document.createElement("span"),
+                p = document.createElement("p");
+                span.setAttribute("id", "me");
+                var docFragment = document.createDocumentFragment();
+                span.appendChild(p);
+                p.appendChild(document.createTextNode(text));
+                docFragment.appendChild(span);
+                chatHistory.appendChild(docFragment);
+
+            console.log(text);
+        };
 
 }());
